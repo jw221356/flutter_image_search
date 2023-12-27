c06ad795fc3f584e5072bae611788dffae865289
jeong won <jw221356@naver.com>
2023-12-27 14:56:21 +0900

    UI 디자인 완성

---
+++ lib/ui/main_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_image_search/data/model/image_item.dart';
+import 'widget/image_item_widget.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    // 둥글게 만들기 위해 BorderRadius 설정
+                    borderSide: const BorderSide(
+                      width: 2,
+                      color: Color(0xFF4FB6B2), // 외곽선 컬러 설정
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    // 둥글게 만들기 위해 BorderRadius 설정
+                    borderSide: const BorderSide(
+                      width: 2,
+                      color: Color(0xFF4FB6B2), // 외곽선 컬러 설정
+                    ),
+                  ),
+                  hintText: 'Search',
+                  suffixIcon: const Icon(
+                    Icons.search,
+                    color: Color(0xFF4FB6B2), // 외곽선 컬러 설정
+                  ),
+                ),
+              ),
+              const SizedBox(height: 24),
+              Expanded(
+                child: GridView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    final imageItem = ImageItem(
+                      imageUrl:
+                      'https://cdn.pixabay.com/photo/2017/09/26/13/21/apples-2788599_150.jpg',
+                      tags: 'apple',
+                    );
+                    return ImageItemWidget(imageItem: imageItem);
+                  },
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 32,
+                    mainAxisSpacing: 32,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
