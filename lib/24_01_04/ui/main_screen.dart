8cf3c6712ae79b8cc275e6b7e41821209cdacc78
jeong won <jw221356@naver.com>
2024-01-04 20:45:55 +0900

    가짜 데이터 표시되도록 Repository 작성 ㅎ

--- lib/24_01_04/ui/main_screen.dart
+++ lib/24_01_04/ui/main_screen.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_image_search/24_01_04/data/repository/image_item_repository.dart';
 import 'package:flutter_image_search/24_01_04/ui/widget/image_item_widget.dart';
 import 'package:flutter_image_search/24_01_04/data/model/image_item.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final repository = ImageItemRepository();
+
+  var imageItems=[];
+
+  Future<void> searchImage(String query) async {
+    imageItems = await repository.getImageItems(query);
+
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,17 +45,22 @@ class MainScreen extends StatelessWidget {
                       color:Color(0xFF4FB6B2),
                     ),
                   ),
+                  hintText: 'Search',
+                  suffixIcon: IconButton(
+                    icon: const Icon(
+                      Icons.search,
+                      color: Color(0xFF4FB6B2),
+                    ),
+                    onPressed: () => searchImage('사과'),
+                  ),
                 ),
               ),
               const SizedBox(height: 24),
               Expanded(
                 child: GridView.builder(
-                  itemCount: 10,
+                  itemCount: imageItems.length,
                   itemBuilder: (context, index) {
-                    final imageItem = ImageItem(
-                      imageUrl: 'https://cdn.pixabay.com/photo/2017/09/26/13/21/apples-2788599_150.jpg',
-                      tags: 'apple',
-                    );
+                    final imageItem = imageItems[index];
                     return ImageItemWidget(imageItem: imageItem);
                   },
                   gridDelegate: const
